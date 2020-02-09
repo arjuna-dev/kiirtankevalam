7c3c40c07316fcb231e26861ed86b02af39bbb14
jandrocamus <arjuna1@protonmail.com>
2020-02-09 08:33:02 +0100

    Minimal changes for Heroku deployment

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from .secrets import secretKey
+# from .secrets import secretKey
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secretKey
+# SECRET_KEY = 'secretKey'
+SECRET_KEY = '*ltx88&dn+^--qpqt0n-wty9v4unn^(b@8a%8-v=g86a&)b(j3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
