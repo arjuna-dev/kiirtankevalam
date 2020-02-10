08d657eac9462803a678951f25c78b93d984d03e
jandrocamus <arjuna1@protonmail.com>
2020-02-10 10:52:03 +0100

    Try deployment on Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ebdjango.wsgi --log-file -
--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -13,9 +13,6 @@ import django_heroku
 import os
 # from .secrets import secretKey
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -148,10 +145,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
